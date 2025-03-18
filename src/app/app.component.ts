be1bcc57347a527fbeec3e45e3d38a7a9e3c47ce
Behzod <behzodsafarov73@gmail.com>
2025-03-18 23:10:39 +0500

    commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HomePageComponent } from "./features/home/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterModule, FormsModule],
+  imports: [HeaderComponent, RouterModule, FormsModule, HomePageComponent],
   template: `
     <app-header></app-header>
+    <app-home-page></app-home-page>
+
   `
 })
 export class AppComponent {}
