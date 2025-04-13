17950e364bce846d97b8da27601d2498bd549d09
unknown <@NETWORK.LOCAL>
2025-04-13 21:19:29 +0300

    sdf

--- src/app/core/constants/api_urls.ts
+++ src/app/core/constants/api_urls.ts
@@ -1,5 +1,5 @@
 export const API_URLS = {
-  BASE_URL: 'https://aksiyona.uz',
+  BASE_URL: 'https://api.aksiyona.uz',
   API_URLS: 'https://api.aksiyona.uz/api',
   ISSUER: 'https://api.aksiyona.uz',
   AUDIENCE: 'https://aksiyona.uz'
