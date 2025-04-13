ed529b557a5c6890cfbdbd5be698df7a23649785
Behzod <behzodsfary@gmail.com>
2025-04-14 00:29:54 +0500

    sdf

--- src/app/core/constants/api_urls.ts
+++ src/app/core/constants/api_urls.ts
@@ -4,3 +4,10 @@ export const API_URLS = {
   ISSUER: 'https://api.aksiyona.uz',
   AUDIENCE: 'https://aksiyona.uz'
 };
+
+// export const API_URLS = {
+//   BASE_URL: 'http://localhost:5251',
+//   API_URLS: 'http://localhost:5251/api',
+//   ISSUER: 'http://localhost:5251',
+//   AUDIENCE: 'http://localhost:5251'
+// };
